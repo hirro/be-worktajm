99f762384307ae062e579f57280fd3fdbb8e67d2
Jim Arnell <jim@arnellconsulting.com>
2013-07-02 09:33:22 +0200

    no message

---
+++ src/main/java/com/arnellconsulting/tps/web/RegistrationController.java
@@ -0,0 +1,28 @@
+package com.arnellconsulting.tps.web;
+
+import javax.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@Slf4j
+public class RegistrationController {
+    @RequestMapping("/registration/register")
+    public String register() {
+       log.debug("Hej");
+        return null;
+
+    }
+}
