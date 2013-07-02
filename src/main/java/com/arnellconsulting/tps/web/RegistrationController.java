e9545bc1c4152c8c7331d8beb9915c3b75dc3e8c
Jim Arnell <jim@arnellconsulting.com>
2013-07-02 19:04:52 +0200

    no message

--- src/main/java/com/arnellconsulting/tps/web/RegistrationController.java
+++ src/main/java/com/arnellconsulting/tps/web/RegistrationController.java
@@ -12,17 +12,37 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/registration/")
 @Slf4j
 public class RegistrationController {
-    @RequestMapping("/registration/register")
-    public String register() {
-       log.debug("Hej");
-        return null;
 
-    }
+  @RequestMapping("/")
+  public String index() {
+    log.debug("index");
+    return null;
+  }
+  
+  @RequestMapping("/create")
+  public String create(@RequestParam(value="email", required=false) String email,
+                         @RequestParam(value="password", required=false) String password,
+                         @RequestParam(value="company", required=false) String company) {
+    log.debug("create: email {}, password: {}, company: {}  ", email, password, company);
+    
+    return null;
+  }
+
+  @RequestMapping("/register")
+  public String register(@RequestParam(value="email", required=false) String email,
+                         @RequestParam(value="password", required=false) String password,
+                         @RequestParam(value="company", required=false) String company) {
+    log.debug("register: email {}, password: {}, company: {}  ", email, password, company);
+    
+    return null;
+  }
 }
