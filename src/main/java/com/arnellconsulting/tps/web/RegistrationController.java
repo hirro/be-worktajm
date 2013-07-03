f7b98995f013327251c74d8730773c06332b15b3
Jim Arnell <jim@arnellconsulting.com>
2013-07-03 07:41:21 +0200

    no message

--- src/main/java/com/arnellconsulting/tps/web/RegistrationController.java
+++ src/main/java/com/arnellconsulting/tps/web/RegistrationController.java
@@ -1,48 +1,63 @@
 package com.arnellconsulting.tps.web;
 
-import javax.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.bind.support.SessionStatus;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+
 
 @Controller
 @RequestMapping("/registration/")
 @Slf4j
 public class RegistrationController {
+   @RequestMapping("/")
+   public String index() {
+      log.debug("index");
 
-  @RequestMapping("/")
-  public String index() {
-    log.debug("index");
-    return null;
-  }
-  
-  @RequestMapping("/create")
-  public String create(@RequestParam(value="email", required=false) String email,
-                         @RequestParam(value="password", required=false) String password,
-                         @RequestParam(value="company", required=false) String company) {
-    log.debug("create: email {}, password: {}, company: {}  ", email, password, company);
-    
-    return null;
-  }
+      return null;
+   }
 
-  @RequestMapping("/register")
-  public String register(@RequestParam(value="email", required=false) String email,
-                         @RequestParam(value="password", required=false) String password,
-                         @RequestParam(value="company", required=false) String company) {
-    log.debug("register: email {}, password: {}, company: {}  ", email, password, company);
-    
-    return null;
-  }
+   @RequestMapping("/create")
+   public String create(@RequestParam(
+      value = "email",
+      required = false
+   ) String email, @RequestParam(
+      value = "password",
+      required = false
+   ) String password, @RequestParam(
+      value = "company",
+      required = false
+   ) String company) {
+      log.debug("create: email {}, password: {}, company: {}  ", email, password, company);
+
+      return null;
+   }
+
+   @RequestMapping("/register")
+   public String register(@RequestParam(
+      value = "email",
+      required = false
+   ) String email, @RequestParam(
+      value = "password",
+      required = false
+   ) String password, @RequestParam(
+      value = "company",
+      required = false
+   ) String company) {
+      log.debug("register: email {}, password: {}, company: {}  ", email, password, company);
+
+      return null;
+   }
+
+   @RequestMapping(value = "/checkEmail.do")
+   @ResponseBody
+   public boolean isEmailUnique(HttpServletResponse response, @RequestParam String email) {
+      log.debug("isEmailUnique: {}", email);
+
+      return (email.length() % 2) == 0;
+   }
 }
