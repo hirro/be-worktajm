64adde0dfee10624326d6716f133ed698e31e7e5
Jim Arnell <jim@arnellconsulting.com>
2013-07-15 09:56:59 +0200

    no message

---
+++ src/main/webapp/WEB-INF/tps/app/scripts/app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('tpsApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/main', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/dashboard', {
+        templateUrl: 'views/dashboard.html',
+        controller: 'DashboardCtrl'
+      })
+      .when('/register', {
+        templateUrl: 'views/register.html',
+        controller: 'RegisterCtrl'
+      })
+      .when('/reports', {
+        templateUrl: 'views/reports.html',
+        controller: 'ReportsCtrl'
+      })
+      .otherwise({
+        redirectTo: '/dashboard'
+      });
+  });
