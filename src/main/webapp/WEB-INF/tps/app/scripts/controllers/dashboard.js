399847e5c434054c45af27d7a1a4f032eb3f74ac
Jim Arnell <jim@arnellconsulting.com>
2013-07-15 23:13:20 +0200

    no message

--- src/main/webapp/WEB-INF/tps/app/scripts/controllers/dashboard.js
+++ src/main/webapp/WEB-INF/tps/app/scripts/controllers/dashboard.js
@@ -2,20 +2,27 @@
 
 angular.module('tpsApp')
   .controller('DashboardCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
     $scope.projects = [
-    	{
-    		"name": "Project A",
-    		"customerName": "Corporate A",
-     		"rate": "750"
-     	},
-    		"name": "Project B",
-    		"customerName": "Corporate B",
-     		"rate": "750"
-     	}
-	  ];    
+      // {
+      //   'id': '1',
+      //   'name': 'Project A',
+      //   'customerName': 'Corporate A',
+      //   'rate': '750'
+      // },
+      // {
+      //   'id': '2',
+      //   'name': 'Project B',
+      //   'customerName': 'Corporate B',
+      //   'rate': '750'
+      // },
+      {
+        'id': '3',
+        'name': 'Project C',
+        'customerName': 'Corporate C',
+        'rate': '750'
+      }
+    ];
+    $scope.user = 'jim@arnellconsulting.com';
+    $scope.email = 'jim@arnellconsulting.com';
+    $scope.welcome = 'Test';
   });
