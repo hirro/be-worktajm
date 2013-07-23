f9063db0a811f9d00d0554679009f857f167c8d8
Jim Arnell <jim@arnellconsulting.com>
2013-07-23 14:47:47 +0200

    CRUD project + logback

--- yo/app/scripts/controllers/dashboard.js
+++ yo/app/scripts/controllers/dashboard.js
@@ -7,19 +7,51 @@ angular.module('tpsApp')
 
     $scope.user = {
       email: 'jim@arnellconsulting.com',
-      verified: true,
+      verified: false,
       authenticated: true
     };
+    $scope.project = new Project({
+      name: 'a',
+      rate: 0,
+      description: ''
+    });
 
     // Projects
-    //$scope.projects = tpsStorage.getProjects();
     $scope.projects = Project.query();
-    $scope.addProject = function () {
-      $scope.projects.push({
-        name: 'Project name',
-        customerName: '',
-        rating: '0'
+    $scope.createProject = function () {
+      console.log('createProject(%s)', $scope.project.name);
+      $scope.projects.push($scope.project);
+      $scope.project.$save();
+      $scope.project = new Project();
+    };
+    $scope.getProjectById = function (id) {
+      var item = $.grep($scope.projects, function (e) { return e.id === id; })[0];
+      return item;
+    };
+    $scope.getProjectIndex = function (project) {
+      console.log('getProjectIndex(%s)', id);
+      var i = -1;
+      var id = project.id;
+      $.each($scope.projects, function (index, value) {
+        if (value.id === id) {
+          i = index;
+        }
       });
+      return i;
+    };
+    $scope.removeProject = function (project) {
+      console.log('removeProject(name: %s, id: %d)', project.name, project.id);
+
+      // Update backend
+      project.$delete({id: project.id});
+
+      // Update model (or query when delete suceeds)
+      var i = $scope.getProjectIndex(project);
+      $scope.projects.splice(i, 1);
+    };
+    $scope.updateProject = function (project) {
+      console.log('removeProject(%s)', project.id); 
+      project.$save();
     };
 
     // Time entries
@@ -44,23 +76,16 @@ angular.module('tpsApp')
       });
       $scope.timeEntries.splice(i, 1);
       console.log('Removing index ', i);
-
     };
+    $scope.getTimeEntryById = function (id) {
+      var item = $.grep($scope.timeEntries, function (e) { return e.id === id; })[0];
+      return item;
+    };
+
     $scope.getTotalTime = function () {
       $.each(timeEntries, function (i, val) {
         console.log('Index: %s %s', i, val);
       });
-    };
-
-    // Finders
-    $scope.getProjectById = function (id) {
-      var item = $.grep($scope.projects, function (e) { return e.id === id; })[0];
-      return item;
-    };
-
-    $scope.getTimeEntryById = function (id) {
-      var item = $.grep($scope.timeEntries, function (e) { return e.id === id; })[0];
-      return item;
     };
 
   });
