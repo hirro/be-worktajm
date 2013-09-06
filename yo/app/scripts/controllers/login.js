64b81165406c71ece81cc6a37c2c31ba857b4a4a
Jim Arnell <jim@arnellconsulting.com>
2013-09-06 07:49:21 +0200

    no message

--- yo/app/scripts/controllers/login.js
+++ yo/app/scripts/controllers/login.js
@@ -2,13 +2,23 @@
 'use strict';
 
 angular.module('tpsApp')
-  .controller('LoginCtrl', function ($scope, Restangular) {
+  .controller('LoginCtrl', function ($scope, $rootScope, Restangular, $location, $http) {
 
 	$scope.login = function () {
 
-		Restangular.one('authenticate').get({
+		$scope.token = Restangular.one('authenticate').get({
 			username: $scope.username,
 			password: $scope.password
+		}).then(function(user) {
+			console.log('Successfully authenticated, user: %s', user.name);
+			$rootScope.user = user;
+			// $http.defaults.headers.common['Auth-Token'] = user.token;			
+			// Restangular.setDefaultHeaders({
+			// 	'Auth-Token': user.token
+			// });
+			$location.path( '/dashboard' );
+		}, function() {
+			console.error('Login failed');
 		});
 	};
 });
