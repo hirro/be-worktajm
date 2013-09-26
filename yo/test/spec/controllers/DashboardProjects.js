1d90ebb459eadcd9fcfaec3eb5c362178ed46246
Jim Arnell <jim@arnellconsulting.com>
2013-09-26 08:29:23 +0200

    no message

--- yo/test/spec/controllers/DashboardProjects.js
+++ yo/test/spec/controllers/DashboardProjects.js
@@ -7,27 +7,35 @@ describe('Controller: DashboardProjectsCtrl', function () {
   beforeEach(module('tpsApp'));
 
   var DashboardProjectsCtrl, scope;
+  var projects = [
+    {'id': 1, 'name': 'Project A', 'description': null, 'rate': null, 'new': false},
+    {'id': 2, 'name': 'Project B', 'description': null, 'rate': null, 'new': false},
+    {'id': 3, 'name': 'Project C', 'description': null, 'rate': null, 'new': false}
+  ];
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope, $injector) {
     scope = $rootScope.$new();
     DashboardProjectsCtrl = $controller('DashboardProjectsCtrl', {
       $scope: scope
-    }); 
-    var projects = [
-      {'id':1,'name':'Project A','description':null,'rate':null,'new':false},
-      {'id':2,'name':'Project B','description':null,'rate':null,'new':false},
-      {'id':3,'name':'Project C','description':null,'rate':null,'new':false}
-    ];
+    });
     var project = [];
-    //projectService = $injector.get('ProjectService');
-    //projectService.when
-    DashboardProjectsCtrl.$inject = ['$scope', '$route'];
+    DashboardProjectsCtrl.$inject = ['$scope',  '$route'];
   }));
 
-  it('should retrive a list of projects from ProjectService to scope', function () {
-    expect(scope.updateProject);
+  it('should initialize with empty project list, etc.', function () {
     expect(scope.projects).toBeDefined();
     expect(scope.projects.length).not.toBeDefined();
+  });
+
+  it('should use projects provided from service when receiving "onProjectsRefreshed" event', function () {
+    scope.$broadcast('onProjectsRefreshed', projects);
+    expect(scope.projects).toBeDefined();
+    expect(scope.projects.length).toBe(3);
+  });
+
+  it('should return the reference of the project with id', function () {
+    scope.projects = projects;
+    expect(DashboardProjectsCtrl.getById(2)).toEqual(projects[1]);
   });
 });
