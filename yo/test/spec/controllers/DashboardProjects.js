940bdd16d03a157b7d4b52b55f0c7194a26f5e10
Jim Arnell <jim@arnellconsulting.com>
2013-09-26 06:55:29 +0200

    no message

--- yo/test/spec/controllers/DashboardProjects.js
+++ yo/test/spec/controllers/DashboardProjects.js
@@ -13,19 +13,21 @@ describe('Controller: DashboardProjectsCtrl', function () {
     scope = $rootScope.$new();
     DashboardProjectsCtrl = $controller('DashboardProjectsCtrl', {
       $scope: scope
-    });
-    projects = [
+    }); 
+    var projects = [
       {'id':1,'name':'Project A','description':null,'rate':null,'new':false},
       {'id':2,'name':'Project B','description':null,'rate':null,'new':false},
       {'id':3,'name':'Project C','description':null,'rate':null,'new':false}
     ];
-    project = [];
-    projectService = $injector.get('ProjectService');
-    projectService.when
+    var project = [];
+    //projectService = $injector.get('ProjectService');
+    //projectService.when
+    DashboardProjectsCtrl.$inject = ['$scope', '$route'];
   }));
 
-  it('should attach a list of projects to scope', function () {
+  it('should retrive a list of projects from ProjectService to scope', function () {
     expect(scope.updateProject);
-    expect(mock.ProjectService.update).toHaveBeenCalled();
+    expect(scope.projects).toBeDefined();
+    expect(scope.projects.length).not.toBeDefined();
   });
 });
--- yo/test/spec/services/personService.js
+++ yo/test/spec/services/personService.js
@@ -2,17 +2,4 @@
 
 describe('Service: personService', function () {
 
-  // load the service's module
-  beforeEach(module('tpsApp'));
-
-  // instantiate service
-  var personService;
-  beforeEach(inject(function (_personService_) {
-    personService = _personService_;
-  }));
-
-  it('should do something', function () {
-    expect(!!personService).toBe(true);
-  });
-
 });
