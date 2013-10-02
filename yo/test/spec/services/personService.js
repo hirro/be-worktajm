476a3ce8254027bf32f179c497b22223568fefa7
Jim Arnell <jim@arnellconsulting.com>
2013-10-02 18:26:39 +0200

    no message

--- yo/test/spec/services/personService.js
+++ yo/test/spec/services/personService.js
@@ -6,40 +6,57 @@ describe('Service: personService', function () {
 
   // API
   var httpBackend;
-  var PersonService, persons;
+  var personSvc, persons;
   var Restangular;
   var scope;
+
+  // Test constants
+  var persons = [
+    { id: 1, username: 'User A'},
+    { id: 2, username: 'User B'}
+  ];
 
   // load the service's module
   beforeEach(module('tpsApp'));
 
-  // Init HTTP mock backend
-  beforeEach(inject(function($injector, _$httpBackend_, $rootScope, _Restangular_) {
-    httpBackend = _$httpBackend_;
-    //httpBackend.expectGET('../json/community/getSearchResults').respond('[{"id":"tester"},{"username":"tester2"}]');
-    Restangular = _Restangular_;
-    scope = $rootScope.$new();
-
-    // Model
-    persons = [
-      { id: 1, email: 'alice@example.com' },
-      { id: 2, email: 'bob@example.com' },
-    ];
-    httpBackend.whenGET('/person/1').respond(persons[0]);
-
-    // Service
-    
+  // Inject the wanted responses into httpBackend
+  beforeEach(inject(function ($httpBackend) {
+    $httpBackend.whenGET('/api/types/ferrari/1').respond(persons[0]);    
   }));
+
+  // Inject the person service
+  inject(function($httpBackend, PersonService) {
+    personSvc = PersonService;
+    httpBackend = $httpBackend;
+  });  
+  // // Inject the used services
+  // beforeEach(inject(function(_$httpBackend_, $rootScope, _Restangular_) {
+  //   httpBackend = _$httpBackend_;
+  //   //httpBackend.expectGET('../json/community/getSearchResults').respond('[{"id":"tester"},{"username":"tester2"}]');
+  //   Restangular = _Restangular_;
+  //   scope = $rootScope.$new();
+
+  //   // Model
+  //   persons = [
+  //     { id: 1, email: 'alice@example.com' },
+  //     { id: 2, email: 'bob@example.com' },
+  //   ];
+  //   httpBackend.whenGET('/person/1').respond(persons[0]);
+
+  //   // Service
+
+  // }));
 
   afterEach(function () {
     httpBackend.verifyNoOutstandingExpectation();
     httpBackend.verifyNoOutstandingRequest();
   });
 
-  it('set active time entry', function () {
-    spyOn(PersonService, 'getPerson').andCallThrough();
-    var person = PersonService.getPerson();
-    expect(PersonService.getPerson).toHaveBeenCalled();
+  it('should get the currenly logged in person', function () {
+    console.log('XxxxxXX');
+    //spyOn(PersonService, 'getPerson').andCallThrough();
+    var person = personSvc.getPerson();
+    //expect(PersonService.getPerson).toHaveBeenCalled();
 
     var resolvedValue;
     person.then(function (pr) {
