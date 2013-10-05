fee43f591dc57c6befd15934bbf375cd8aba4a27
Jim Arnell <jim@arnellconsulting.com>
2013-10-05 13:58:12 +0200

    no message

--- yo/test/spec/services/personService.js
+++ yo/test/spec/services/personService.js
@@ -1,74 +1,101 @@
-/* globals expect, $httpBackend, it, beforeEach, describe, inject */
+/* globals expect, it, afterEach, beforeEach, spyOn, describe, inject */
 
 'use strict';
 
 describe('Service: personService', function () {
 
-  // API
-  var httpBackend;
-  var personSvc, persons;
-  var Restangular;
-  var scope;
-
-  // Test constants
-  var persons = [
-    { id: 1, username: 'User A'},
-    { id: 2, username: 'User B'}
-  ];
-
   // load the service's module
   beforeEach(module('tpsApp'));
 
-  // Inject the wanted responses into httpBackend
-  beforeEach(inject(function ($httpBackend) {
-    $httpBackend.whenGET('/api/types/ferrari/1').respond(persons[0]);    
-  }));
+  // API
+  var httpBackend;
+  var service;
+  var scope;
+
+  // Test constants
+  var projects = [
+    { id: 1, name: 'Project A', rate: null }];
+  var timeEntries = [
+    { id: 1, startTime: 0, endTime: 2, project: projects[0] }];
+  var persons = [
+    { id: 1, username: 'User A', activeTimeEntry: null },
+    { id: 2, username: 'User B' },
+    { id: 3, username: 'User C', activeTimeEntry: timeEntries[0] }];
 
   // Inject the person service
-  inject(function($httpBackend, PersonService) {
-    personSvc = PersonService;
+  beforeEach(inject(function (PersonService, $httpBackend, $rootScope) {
+    service = PersonService;
     httpBackend = $httpBackend;
-  });  
-  // // Inject the used services
-  // beforeEach(inject(function(_$httpBackend_, $rootScope, _Restangular_) {
-  //   httpBackend = _$httpBackend_;
-  //   //httpBackend.expectGET('../json/community/getSearchResults').respond('[{"id":"tester"},{"username":"tester2"}]');
-  //   Restangular = _Restangular_;
-  //   scope = $rootScope.$new();
-
-  //   // Model
-  //   persons = [
-  //     { id: 1, email: 'alice@example.com' },
-  //     { id: 2, email: 'bob@example.com' },
-  //   ];
-  //   httpBackend.whenGET('/person/1').respond(persons[0]);
-
-  //   // Service
-
-  // }));
+    scope = $rootScope;
+  }));  
 
   afterEach(function () {
     httpBackend.verifyNoOutstandingExpectation();
     httpBackend.verifyNoOutstandingRequest();
   });
 
-  it('should get the currenly logged in person', function () {
-    console.log('XxxxxXX');
-    //spyOn(PersonService, 'getPerson').andCallThrough();
-    var person = personSvc.getPerson();
-    //expect(PersonService.getPerson).t©oHaveBeenCalled();
+  describe('getPerson', function () {
 
-    var resolvedValue;
-    person.then(function (pr) {
-      resolvedValue = pr;
+    it('should get the currenly logged in person', function () {
+
+      // Test setup
+      httpBackend.whenGET('http://localhost:8080/api/api/person/1').respond(persons[0]);
+      spyOn(service, 'getPerson').andCallThrough();
+
+      // Test
+      var person = null;
+      service.getPerson().then(function (result) {
+        person = result;
+      });
+
+      // Make the requests go though
+      scope.$digest();
+      httpBackend.flush();
+
+      expect(service.getPerson).toHaveBeenCalled();
+      expect(person).toBeDefined();
+      expect(person.id).toBe(1);
+      expect(person.username).toBeDefined();
+      expect(person.username).toBe('User A');
     });
 
-    httpBackend.flush();
-    //person.resolve();
-    expect(person).toBeDefined();
-    expect(person.id).toBe(1);
-    expect(person.username).toBeDefined();
-    expect(person.username).toBe('alice@example.com');
+    it('should fail gracefully when token has expired or is invalid', function () {
+
+      // Test setup
+      httpBackend.whenGET('http://localhost:8080/api/api/person/1').respond(403);
+      spyOn(service, 'getPerson').andCallThrough();
+
+      // Test
+      var person = null;
+      service.getPerson().then(function (result) {
+        person = result;
+      }, function () {
+        person = null;
+      });
+
+      // Make the requests go though
+      scope.$digest();
+      httpBackend.flush();
+
+      expect(service.getPerson).toHaveBeenCalled();
+      expect(person).toBe(null);
+    });
+
   });
 
+  describe('getActiveProjectId', function () {
+
+    it('should return the -1 when no project is active', function () {
+      httpBackend.whenGET('http://localhost:8080/api/api/person/1').respond(persons[0]);
+      var projectId = service.getActiveProjectId();
+      expect(projectId).toBe(-1);
+    });
+
+    it('should return the project id when project is active', function () {
+      httpBackend.whenGET('http://localhost:8080/api/api/person/3').respond(null);
+      var projectId = service.getActiveProjectId();
+      expect(projectId).toBe(-1);
+    });
+
+  });
 });
