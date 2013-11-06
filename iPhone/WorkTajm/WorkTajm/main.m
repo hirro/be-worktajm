1a5df91af7f8739c861e93f9e1b95a090fc7f003
Jim Arnell <jim@arnellconsulting.com>
2013-11-06 21:03:26 +0100

    New iPhone project added

---
+++ iPhone/WorkTajm/WorkTajm/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WorkTajm
+//
+//  Created by Jim Arnell on 06/11/13.
+//  Copyright (c) 2013 Jim Arnell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "wtajmAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([wtajmAppDelegate class]));
+    }
+}
---
+++ iPhone/WorkTajm/WorkTajm/wtajmAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  wtajmAppDelegate.h
+//  WorkTajm
+//
+//  Created by Jim Arnell on 06/11/13.
+//  Copyright (c) 2013 Jim Arnell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface wtajmAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
