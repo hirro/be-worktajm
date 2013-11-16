c055cbdbb6120a27fcb9a87fc718d8051a53c3c7
Jim Arnell <jim@arnellconsulting.com>
2013-11-16 15:50:38 +0100

    Moved JS to own project

--- yo/app/scripts/controllers/Main.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-angular.module('tpsApp')
-  .controller('MainCtrl', function ($scope) {
-  });
--- yo/app/scripts/services/TpsConstants.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-angular.module('tpsApp')
-  .constant('TpsConstants', 42);
