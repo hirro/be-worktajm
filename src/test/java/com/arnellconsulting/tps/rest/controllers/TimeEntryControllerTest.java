af689ef113e9c0a00ff1bd125423f0d1e07db015
Jim Arnell <jim@arnellconsulting.com>
2014-04-21 07:04:20 +0200

    Time compare failed

--- src/test/java/com/arnellconsulting/tps/rest/controllers/TimeEntryControllerTest.java
+++ src/test/java/com/arnellconsulting/tps/rest/controllers/TimeEntryControllerTest.java
@@ -18,12 +18,6 @@
 
 package com.arnellconsulting.tps.rest.controllers;
 
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
-
 import com.arnellconsulting.tps.common.TestConstants;
 import com.arnellconsulting.tps.config.TestContext;
 import com.arnellconsulting.tps.config.WebAppContext;
@@ -32,27 +26,37 @@ import com.arnellconsulting.tps.model.Project;
 import com.arnellconsulting.tps.model.TimeEntry;
 import com.arnellconsulting.tps.security.PersonUserDetails;
 import com.arnellconsulting.tps.service.TpsService;
-
+import java.util.ArrayList;
+import java.util.List;
+import static org.hamcrest.Matchers.is;
+import org.joda.time.DateTime;
+import static org.junit.Assert.assertThat;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
-
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-
-import java.util.ArrayList;
-import java.util.List;
-import org.joda.time.DateTime;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
 /**
  *
@@ -185,7 +189,7 @@ public class TimeEntryControllerTest {
       )
       .andExpect(status().isOk())
       .andExpect(content().contentType(TestConstants.APPLICATION_JSON_UTF8))
-      .andExpect(jsonPath("startTime", is("2010-05-20T22:00:00Z")));
+      .andExpect(jsonPath("startTime", is(timeEntryA.getStartTime().toInstant().toString())));
 
       verify(tpsServiceMock, times(1)).getTimeEntry(1L);
       verifyNoMoreInteractions(tpsServiceMock);
